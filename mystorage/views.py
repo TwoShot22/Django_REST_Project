1f560f94b5936da7137aaccfd347e4da3bac9324
Mansoo Kim <mansoo0621@mju.ac.kr>
2020-02-29 00:19:15 +0900

    Create Album&Files Model / Media Setting

--- mystorage/views.py
+++ mystorage/views.py
@@ -1,10 +1,14 @@
 from rest_framework import viewsets
 from .models import Essay
 from .serializers import EssaySerializer
+from rest_framework.filters import SearchFilter
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Essay.objects.all()
     serializer_class = EssaySerializer
+
+    filter_backends = [SearchFilter]
+    search_fields = ('title', 'body','author_name')
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
