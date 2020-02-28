3c90fa953eca6dd9091dcc95c13cb4cbeeccb381
Mansoo Kim <mansoo0621@mju.ac.kr>
2020-02-28 23:39:56 +0900

    Create Essay Model

--- mystorage/views.py
+++ mystorage/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Essay
+# from .serializers import Essayserializers
 
-# Create your views here.
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Essay.objects.all()
+    # serializer_class = Essayserializers
+
